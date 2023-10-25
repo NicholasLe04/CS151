105d62b54bc113f7fc40a43e2016824893b2f35c
Alex Bui <112739079+al3xbro@users.noreply.github.com>
2023-10-25 15:46:29 -0700

    Accidentally pushed to this branch (#3)
    * Add interfaces, no functionality
    
    * Add styling and organize
    
    * Add cancel button functionality
    
    * Add Create functionality
    
    * Add project display and reorganize
    
    * Remove old application
    
    * Add project stomp and projectCard CSS
    
    * Add project stomp and projectCard CSS
    
    * Organize imports
    
    * Add ticket display

--- src/application/controller/ticket/TicketController.java
+++ src/application/controller/ticket/TicketController.java
@@ -1,5 +1,33 @@
 package application.controller.ticket;
+import java.sql.Connection;
+
+import db.SQLConnector;
+import entries.CommentDAO;
+import entries.TicketDAO;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class TicketController {
-
+	
+	private Connection conn;
+	private TicketDAO ticketDAO;
+	private CommentDAO commentDAO;
+	
+	@FXML public Label title;
+	@FXML public Label desc;
+	
+	@FXML
+	public void initialize() {
+		conn = new SQLConnector().getConnection();
+		ticketDAO = new TicketDAO(conn);
+		commentDAO = new CommentDAO(conn);
+	}
+	
+	public void setTitle(String title) {
+		this.title.setText(title);
+	}
+	
+	public void setDesc(String desc) {
+    	this.desc.setText(desc);
+    }
 }
